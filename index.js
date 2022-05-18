3b1a98611784a30e4a11ff7e992e8a4f31944f8c
root <vineela.tarra@gmail.com>
2022-05-18 09:07:25 +0000

    react2

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  // Write your code here.
+  <div className="backGround">
+    <h1 className="heading">Congratulations</h1>
+    <div className="backGround1">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="heading">Kiran V</h1>
+      <p className="paragraph">
+        Vishnu institute of computer education & technology,Bheemavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
